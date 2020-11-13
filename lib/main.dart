a3e2a665b83bd03fed35e66afb5ade44c68d3210
FlutLab.io <>
2020-11-13 05:27:57 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedloginscreenwidget/GeneratedLOGINSCREENWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedloginscreenwidget1/GeneratedLOGINSCREENWidget1.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedhomescreenwidget/GeneratedHOMESCREENWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedsplashscreenwidget/GeneratedSPLASHSCREENWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatednavigationscreenwidget/GeneratedNAVIGATIONSCREENWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedprofilempyascreenwidget/GeneratedPROFILEMPYASCREENWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedcreatetask2screenwidget/GeneratedCREATETASK2SCREENWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedreportscreenwidget/GeneratedREPORTSCREENWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedtargetscreenwidget/GeneratedTARGETSCREENWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedreporttabsscreenwidget/GeneratedREPORTTABSSCREENWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedopiniontabsscreenwidget/GeneratedOPINIONTABSSCREENWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedpermissionrequestpagewidget/GeneratedPERMISSIONREQUESTPAGEWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedcreatetargetpagewidget/GeneratedCREATETARGETPAGEWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedpermissionpagewidget/GeneratedPERMISSIONPAGEWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedpasswordsettingpagewidget/GeneratedPASSWORDSETTINGPAGEWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedcreateopinionpagewidget/GeneratedCREATEOPINIONPAGEWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedlisttargetspagewidget/GeneratedLISTTARGETSPAGEWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedlistreportpagewidget/GeneratedLISTREPORTPAGEWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedlistreportpagewidget1/GeneratedLISTREPORTPAGEWidget1.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedlistopinionpagewidget/GeneratedLISTOPINIONPAGEWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedopinionviewpagewidget/GeneratedOPINIONVIEWPAGEWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedallreportpagewidget/GeneratedALLREPORTPAGEWidget.dart';
+import 'package:flutterapp/jatu_20report_20systemapp/generatedcommentedreportwidget/GeneratedCOMMENTEDREPORTWidget.dart';
+
+void main() {
+  runApp(JATU_20REPORT_20SYSTEMApp());
+}
+
+class JATU_20REPORT_20SYSTEMApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedPROFILEMPYASCREENWidget',
+      routes: {
+        '/GeneratedLOGINSCREENWidget': (context) =>
+            GeneratedLOGINSCREENWidget(),
+        '/GeneratedLOGINSCREENWidget1': (context) =>
+            GeneratedLOGINSCREENWidget1(),
+        '/GeneratedHOMESCREENWidget': (context) => GeneratedHOMESCREENWidget(),
+        '/GeneratedSPLASHSCREENWidget': (context) =>
+            GeneratedSPLASHSCREENWidget(),
+        '/GeneratedNAVIGATIONSCREENWidget': (context) =>
+            GeneratedNAVIGATIONSCREENWidget(),
+        '/GeneratedPROFILEMPYASCREENWidget': (context) =>
+            GeneratedPROFILEMPYASCREENWidget(),
+        '/GeneratedCREATETASK2SCREENWidget': (context) =>
+            GeneratedCREATETASK2SCREENWidget(),
+        '/GeneratedREPORTSCREENWidget': (context) =>
+            GeneratedREPORTSCREENWidget(),
+        '/GeneratedTARGETSCREENWidget': (context) =>
+            GeneratedTARGETSCREENWidget(),
+        '/GeneratedREPORTTABSSCREENWidget': (context) =>
+            GeneratedREPORTTABSSCREENWidget(),
+        '/GeneratedOPINIONTABSSCREENWidget': (context) =>
+            GeneratedOPINIONTABSSCREENWidget(),
+        '/GeneratedPERMISSIONREQUESTPAGEWidget': (context) =>
+            GeneratedPERMISSIONREQUESTPAGEWidget(),
+        '/GeneratedCREATETARGETPAGEWidget': (context) =>
+            GeneratedCREATETARGETPAGEWidget(),
+        '/GeneratedPERMISSIONPAGEWidget': (context) =>
+            GeneratedPERMISSIONPAGEWidget(),
+        '/GeneratedPASSWORDSETTINGPAGEWidget': (context) =>
+            GeneratedPASSWORDSETTINGPAGEWidget(),
+        '/GeneratedCREATEOPINIONPAGEWidget': (context) =>
+            GeneratedCREATEOPINIONPAGEWidget(),
+        '/GeneratedLISTTARGETSPAGEWidget': (context) =>
+            GeneratedLISTTARGETSPAGEWidget(),
+        '/GeneratedLISTREPORTPAGEWidget': (context) =>
+            GeneratedLISTREPORTPAGEWidget(),
+        '/GeneratedLISTREPORTPAGEWidget1': (context) =>
+            GeneratedLISTREPORTPAGEWidget1(),
+        '/GeneratedLISTOPINIONPAGEWidget': (context) =>
+            GeneratedLISTOPINIONPAGEWidget(),
+        '/GeneratedOPINIONVIEWPAGEWidget': (context) =>
+            GeneratedOPINIONVIEWPAGEWidget(),
+        '/GeneratedALLREPORTPAGEWidget': (context) =>
+            GeneratedALLREPORTPAGEWidget(),
+        '/GeneratedCOMMENTEDREPORTWidget': (context) =>
+            GeneratedCOMMENTEDREPORTWidget(),
+      },
+    );
+  }
+}
